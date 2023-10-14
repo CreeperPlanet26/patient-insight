675f1e092e17b255b6fbbc1c6f0c5a6f45cd7592
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2023-10-14 13:39:49 -0400

    landing page for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { PatientIcon } from "@/icons/PatientIcon";
+import Image from "next/image";
+import { Button } from "@mui/material"
+
 export default function Home() {
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <div className="home-page">
+      <nav>
+        <Button className="patient-btn">
+          <PatientIcon /> Patient Insight</Button>
+      </nav>
+
+      <h1>Diagnose in seconds</h1>
+      <p>Lorem ipsum dolor sit amet consectetur. Nibh augue sed ornare in cras malesuada. Malesuada bibendum tristique vitae aenean ac. </p>
+
+
+      <Button className="try-btn" variant="contained" color="primary" target="_blank" href="/">
+        Try now
+      </Button>
+      <Image src="/medical-team.png" alt="Medical Team" width={512} height={512} />
+    </div>
   )
 }
