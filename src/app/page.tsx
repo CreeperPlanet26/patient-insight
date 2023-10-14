d68f33c03712b89aff8815c3b37da259482b0168
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2023-10-14 14:46:14 -0400

    responsive landing page for desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,14 @@ export default function Home() {
           <PatientIcon /> Patient Insight</Button>
       </nav>
 
-      <h1>Diagnose in seconds</h1>
-      <p>Lorem ipsum dolor sit amet consectetur. Nibh augue sed ornare in cras malesuada. Malesuada bibendum tristique vitae aenean ac. </p>
+      <section>
+        <h1>Diagnose in seconds</h1>
+        <p>Lorem ipsum dolor sit amet consectetur. Nibh augue sed ornare in cras malesuada. Malesuada bibendum tristique vitae aenean ac. </p>
+        <Button className="try-btn" variant="contained" color="primary" target="_blank" href="/">
+          Try now
+        </Button>
+      </section>
 
-
-      <Button className="try-btn" variant="contained" color="primary" target="_blank" href="/">
-        Try now
-      </Button>
       <Image src="/medical-team.png" alt="Medical Team" width={512} height={512} />
     </div>
   )
